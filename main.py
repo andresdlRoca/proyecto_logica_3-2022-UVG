78addb2c69ce5f8f7de4c8deeae9275e58b2b8fd
andresdlRoca <andresdelaroca11@gmail.com>
2022-10-03 19:06:40 -0600

    Validacion de contenido de columna

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import csv
 import pandas as pd
 
+#Validacion de header por medio de regex
 def validarHeader(line):
     headers = []
     reHeader = re.compile(r"[a-zA-Z]+") #Regex utilizado para validar headers
@@ -11,25 +12,38 @@ def validarHeader(line):
         headers.append(headerName)
     return headers
 
+#Validacion de contenido por medio de regex
 def validarContenido(line):
-    pass
+    content = []
+    reContent = re.compile(r'''(?:,|\n|^)("(?:(?:"")*[^"]*)*"|[^",\n]*|(?:\n|$))''')
+    for word in line:
+        contentName = reContent.match(word).group(0)
+        content.append(contentName)
+    return content
 
+#El .csv se coloca dentro de una lista
 with open('BL-Flickr-Images-Book.csv', newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     data = list(reader)
 
 noColumns = len(data[0])
+content = []
 
 #Validacion de header con regex
-# ...
 for row in data:
     headers = validarHeader(row)
     if len(headers) == noColumns:
         break
 
-print(headers)
-
+del data[0]
 #Validacion de contenido de columna
-# ...
+for row in data:
+    try:
+        content.append(validarContenido(row))
+    except:
+        print(row)
 
+
+
+    
 
